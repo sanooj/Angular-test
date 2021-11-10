0c4492e67951dacf4ce9ad9853e59d548855a890
Sanooj <sanooj007@gmail.com>
2021-11-10 13:34:38 +0530

    Basic setup and modules integrated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { SwiperModule } from 'swiper/angular';
+import { LightboxModule } from 'ngx-lightbox';
+
 
 import { AppComponent } from './app.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FontAwesomeModule,
+    SwiperModule,
+    LightboxModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
